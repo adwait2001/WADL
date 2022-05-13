5b85ecac3e6f7d402c39dd02755f3762dcb10f37
adwait2001 <adwaitnsk.2009@rediffmail.com>
2022-05-13 11:08:56 +0530

    tp

--- Table_Angular/Table/src/app/user/user.component.ts
+++ Table_Angular/Table/src/app/user/user.component.ts
@@ -12,25 +12,32 @@ export class UserComponent implements OnInit {
   value2='';
   public matrix1:Number[]=[];
   public matrix2:Number[]=[];
+  public matrix3:Number[]=[];
 
   handleAdd=()=>{
       var l1 = this.value1.split('#')
       var l2 = this.value2.split('#')
       var x = parseInt(l1[0])
       var y = parseInt(l2[0])
+      let x1=parseInt(l1[2])
+      let y1=parseInt(l2[2])
       for (let i = 0; i < x*x; i++) {
-          this.matrix1.push(i+x)
+          this.matrix1.push(i+x1)
       }
 
       for (let j = 0; j < y*y; j++) {
-        this.matrix2.push(j+y)
+        this.matrix2.push(j+y1)
       }
+
+
+
   }
 
   isDone=(value : any)=>{
     var l1 = this.value1.split('#')
     var x = parseInt(l1[0])
-    if((value-x)%x==x-1)
+    var y = parseInt(l1[2])
+    if((value-x+(x-y))%x==x-1)
     {
       return false;
     }
@@ -40,7 +47,8 @@ export class UserComponent implements OnInit {
   isDone2=(value : any)=>{
     var l1 = this.value2.split('#')
     var x = parseInt(l1[0])
-    if((value-x)%x==x-1)
+    var y = parseInt(l1[2])
+    if((value-x+(x-y))%x==x-1)
     {
       return false;
     }
